28765a7a55564267455a52ac5aaf1a589b1fae9b
Yan-Yi Li <liyanyi@gmail.com>
2016-07-19 11:43:14 +0100

    add coveralls badge in README

---
+++ README.md
@@ -0,0 +1,2 @@
+# Checkout
+[![Coverage Status](https://coveralls.io/repos/github/yyl29/checkout/badge.svg?branch=master)](https://coveralls.io/github/yyl29/checkout?branch=master)
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -9,4 +9,13 @@ describe Checkout do
       expect(checkout.basket).to eq [1]
     end
   end
+
+  describe '#basket' do
+    it 'stores all items added' do
+      checkout.scan(1)
+      checkout.scan(2)
+      checkout.scan(3)
+      expect(checkout.basket).to eq [1, 2, 3]
+    end
+  end
 end
